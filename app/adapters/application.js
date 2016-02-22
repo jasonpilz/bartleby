1051a8e1e4f9f20b5748c4b2750e5d6c1e47261f
Jason Pilz <jasonpilz@gmail.com>
2016-02-22 13:31:27 -0700

    completed initial starting point- removed fakeData

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -5,11 +5,6 @@ const electron = requireNode('electron');
 const mainProcess = electron.remote.require('./electron');
 const filesystem = mainProcess.filesystem;
 
-let fakeData = [
-  { id: 'hello.md', content: 'This is a note.' },
-  { id: 'byebye.md', content: 'This is another note.' },
-];
-
 export default DS.Adapter.extend({
 
   findAll() {
